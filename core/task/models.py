64a7ed8eba23d6e3d79e0ebd49cf8936b1b45db8
Carlos Luna <carlos.lun.salazar@gmail.com>
2022-09-02 21:18:22 -0400

    login funcional

--- core/task/models.py
+++ core/task/models.py
@@ -5,6 +5,7 @@ class Login(models.Model):
     title = models.CharField(max_length=50)
     errorMessage = models.CharField(max_length=100)
     labelButton = models.CharField(max_length=50)
+    objects = models.Manager()
 
     def __str__(self):
         return self.title
--- core/task/views.py
+++ core/task/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Login
+from .serializers import LoginSerializer
 
 # Create your views here.
+class LoginView(generics.ListCreateAPIView):
+    queryset = Login.objects.all()
+    serializer_class = LoginSerializer
+    pass
