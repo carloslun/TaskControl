c864db7f645281a9f0039f1c0c89a1ea2188b9cf
hans <han.godoy@alumnos.duoc.cl>
2022-09-07 21:30:19 -0300

    Add Custom User
    Se añadió un nuevo modulo para django y se modificó la url

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,4 +21,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('task.urls')),
+    path('api/usuario/', include('customUser.urls')),
 ]
